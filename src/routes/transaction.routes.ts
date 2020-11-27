a97ccd6181c9734e45639c319f0f1aecdc0684c7
Marcelo Galdino <marcelog@EQUIP2618.romagnole.local>
2020-11-27 10:53:34 -0300

    challenge finished

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -9,7 +9,10 @@ const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,12 +20,15 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
+    // Recebe os valores do corpo da requisição
     const { title, value, type } = request.body;
 
+    // Instanciamos o nosso service que é responsável pela regra de negócio
     const createTransaction = new CreateTransactionService(
       transactionsRepository,
     );
 
+    // Executamos para criar a transaction
     const transaction = createTransaction.execute({ title, value, type });
 
     return response.json(transaction);
